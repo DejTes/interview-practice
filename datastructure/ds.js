d432e8e6b65f7b6831826fdc093229ae9dba8ef8
dejtes <dejenaref@gmail.com>
2023-06-12 01:45:03 -0400

    Find the largest element in an array

--- datastructure/ds.js
+++ datastructure/ds.js
@@ -17,3 +17,19 @@ function reverseString2(str) {
     return reversed;
 }
 console.log(reverseString2('Hello, World!'));
+
+//2. Find the largest element in an array: Write a function that takes an array of numbers as input and returns the largest number in the array. For example, if the input is [4, 7, 2, 9, 1], the output should be 9.
+
+function largestElement(arr) {
+    let largest = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+// example
+const arr = [4, 7, 2, 9, 11];
+const largest = largestElement(arr);
+console.log(largest)
